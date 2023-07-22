82aae476f88489141c170066a34c2f7ff56d9ea0
KornX_my68@student.itstep.org <KornX_my68@student.itstep.org>
2023-07-22 19:04:45 +0300

    1

--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Text;v
 using System.Threading.Tasks;
 
 namespace ConsoleApp10
@@ -11,6 +11,8 @@ namespace ConsoleApp10
         static void Main(string[] args)
         {
             Console.WriteLine("hi hitler");
+            Console.WriteLine("hi hitler2");
+
         }
     }
 }
